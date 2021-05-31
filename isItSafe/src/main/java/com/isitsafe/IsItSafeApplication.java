a9ed923abab384fbb0b1bc1e37f4974e78476863
Emberdot <michelms02@outlook.com>
2021-05-31 07:58:35 -0300

    Initial Project Commit

---
+++ isItSafe/src/main/java/com/isitsafe/IsItSafeApplication.java
@@ -0,0 +1,13 @@
+package com.isitsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsItSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsItSafeApplication.class, args);
+	}
+
+}
---
+++ isItSafe/src/test/java/com/isitsafe/IsItSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isitsafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsItSafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
